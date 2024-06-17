5c31e35ffed4d706a52b26f279b99bdff6e16ce4
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-06-17 20:34:33 +0900

    [add] サウンドマネージャー
    Play, Pause, Resume, Stop, StopAll を実装。

--- Assets/Scripts/Sound/CriSoundManager.cs
+++ Assets/Scripts/Sound/CriSoundManager.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System;
 using CriWare;
 using System.Collections.Generic;
-using System.Threading;
 
 public class CriSoundManager : SingletonMonoBehaviour<CriSoundManager>
 {
@@ -21,15 +20,61 @@ public class CriSoundManager : SingletonMonoBehaviour<CriSoundManager>
     /// <summary>プレイヤー</summary>
     private CriAtomExPlayer _player = new CriAtomExPlayer();
 
-    /// <summary>キャンセル</summary>
-    private CancellationTokenSource _tokenSource = new CancellationTokenSource();
+    /// <summary>再生音のコレクション</summary>
+    private Dictionary<int, CriAtomExPlayback> _playbackDic = 
+        new Dictionary<int, CriAtomExPlayback>();
 
-    ~CriSoundManager()
+    /// <summary>再生音のID</summary>
+    private static int _nextPlaybackIndex = 0;
+
+    /// <summary>音声を再生してコレクションに登録する</summary>
+    /// <param name="cueSheetName">キューシート名</param>
+    /// <param name="cueName">キュー名</param>
+    /// <param name="volume">ボリューム</param>
+    public void Play(string cueSheetName, string cueName, float volume)
     {
-        // キャンセルを実行
-        _tokenSource.Cancel();
+        // プレイヤーを設定
+        var currentAcb = CriAtom.GetAcb(cueSheetName);
+        _player.SetCue(currentAcb, cueName);
+        _player.SetVolume(volume * _volume);
 
-        // プレイヤーを破棄
-        _player.Dispose();
+        // 再生音を取得
+        var playback = _player.Start();
+
+        // インデックスを更新
+        _nextPlaybackIndex++;
+
+        // 再生音を登録
+        _playbackDic[_nextPlaybackIndex] = playback;
+    }
+
+    /// <summary>再生音をポーズ</summary>
+    /// <param name="index">コレクションのキー</param>
+    public void Pause(int index)
+    {
+        if(_playbackDic.ContainsKey(index)) _playbackDic[index].Pause();
+    }
+
+    /// <summary>再生音をポーズ解除</summary>
+    /// <param name="index">コレクションのキー</param>
+    public void Resume(int index)
+    {
+        if(_playbackDic.ContainsKey(index)) 
+            _playbackDic[index].Resume(CriAtomEx.ResumeMode.AllPlayback);
+    }
+
+    /// <summary>再生音を停止</summary>
+    /// <param name="index">コレクションのキー</param>
+    public void Stop(int index)
+    {
+        if(_playbackDic.ContainsKey(index))
+            _playbackDic[index].Stop(false);
+    }
+
+    /// <summary>全ての再生音を停止</summary>
+    public void StopAll()
+    {
+        _player.Stop(false);
+        _playbackDic.Clear();
     }
 }
